15e8760c59bf7fac79fa95dd6d508756678966b4
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2023-11-26 00:11:40 +0900

    [FEAT] 팀생성하기 Navigationbar 구현

--- ETA/Source/HomeView.swift
+++ ETA/Source/HomeView.swift
@@ -11,95 +11,99 @@ struct HomeView: View {
     let rows = [GridItem(.flexible())]
     let colors: [Color] = [.black, .blue, .brown, .cyan, .gray, .indigo, .mint, .yellow, .orange, .purple]
     var body: some View {
-        VStack{
-        VStack{
-            ZStack{
+        NavigationView {
+            VStack{
                 VStack{
-                    VStack(alignment: .leading){
-                        HStack{
-                            Spacer()
-                            Button(action: {}, label: {
-                                Image("Bell").resizable().frame(width: 24, height: 28).padding(.trailing, 16)
-                            })
+                    ZStack{
+                        VStack{
+                            VStack(alignment: .leading){
+                                HStack{
+                                    Spacer()
+                                    Button(action: {}, label: {
+                                        Image("Bell").resizable().frame(width: 24, height: 28).padding(.trailing, 16)
+                                    })
+                                }
+                                HStack{
+                                    Text("ETA와 함께하는\n즐거운 회의 시간 :)")
+                                        .font(
+                                            Font.custom("Pretendard Variable", size: 24)
+                                                .weight(.bold)
+                                        )
+                                        .kerning(0.024)
+                                        .foregroundColor(.white).padding(.bottom, 83).padding(.leading, 26)
+                                    Spacer()
+                                    
+                                    Image("LogoGrey").padding(.trailing, 0)
+                                }.frame(width: .infinity, height: 150)
+                                Spacer()
+                                
+                                
+                            }.foregroundColor(.clear)
+                                .frame(width: 393, height: 298)
+                                .background(
+                                    LinearGradient(
+                                        stops: [
+                                            Gradient.Stop(color: Color(red: 1, green: 0.44, blue: 0.07), location: 0.00),
+                                            Gradient.Stop(color: Color(red: 1, green: 0.72, blue: 0), location: 1.00),
+                                        ],
+                                        startPoint: UnitPoint(x: 0.35, y: 0.72),
+                                        endPoint: UnitPoint(x: 1.13, y: -0.57)
+                                    )
+                                ).frame(height: 290)
                         }
+                        CollectionViewCell().offset(y: 100)
+                        
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 27, height: 6)
+                            .background(Color(red: 1, green: 0.44, blue: 0.07))
+                            .cornerRadius(30).offset(y: 200)
+                        
+                    }
+                    NavigationLink(destination: MakingTeamView()){
                         HStack{
-                            Text("ETA와 함께하는\n즐거운 회의 시간 :)")
+                            // H1
+                            Text("팀 생성하기")
                                 .font(
-                                    Font.custom("Pretendard Variable", size: 24)
+                                    Font.custom("Pretendard Variable", size: 20)
                                         .weight(.bold)
                                 )
-                                .kerning(0.024)
-                                .foregroundColor(.white).padding(.bottom, 83).padding(.leading, 26)
-                            Spacer()
+                                .kerning(0.02)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color(red: 1, green: 0.44, blue: 0.07)).padding(.trailing, 48).padding(.leading, 26)
                             
-                            Image("LogoGrey").padding(.trailing, 0)
-                        }.frame(width: .infinity, height: 150)
+                            Image("Arrow").padding(.trailing, 34)
+                        }.foregroundColor(.clear)
+                            .frame(width: 360, height: 90)
+                            .background(.white)
+                            .cornerRadius(20)
+                            .shadow(color: Color(red: 0.2, green: 0.12, blue: 0.06).opacity(0.1), radius: 10, x: 5, y: 3)
+                            .padding(.bottom, 16).padding(.top, 70)
+                        
+                    }
+                    
+                    HStack(){
+                        
+                        Text("ETA를 가장 잘 활용하는 방법은 ?")
+                            .font(
+                                Font.custom("Pretendard Variable", size: 20)
+                                    .weight(.bold)
+                            )
+                            .kerning(0.02)
+                            .foregroundColor(.black).padding(.leading, 26)
                         Spacer()
                         
-                        
                     }.foregroundColor(.clear)
-                        .frame(width: 393, height: 298)
-                        .background(
-                            LinearGradient(
-                                stops: [
-                                    Gradient.Stop(color: Color(red: 1, green: 0.44, blue: 0.07), location: 0.00),
-                                    Gradient.Stop(color: Color(red: 1, green: 0.72, blue: 0), location: 1.00),
-                                ],
-                                startPoint: UnitPoint(x: 0.35, y: 0.72),
-                                endPoint: UnitPoint(x: 1.13, y: -0.57)
-                            )
-                        ).frame(height: 290)
-                }
-                CollectionViewCell().offset(y: 100)
-                
-                Rectangle()
-                .foregroundColor(.clear)
-                .frame(width: 27, height: 6)
-                .background(Color(red: 1, green: 0.44, blue: 0.07))
-                .cornerRadius(30).offset(y: 200)
-            
+                        .frame(width: 360, height: 90)
+                        .background(.white)
+                        .cornerRadius(20)
+                        .shadow(color: Color(red: 0.2, green: 0.12, blue: 0.06).opacity(0.1), radius: 10, x: 5, y: 3)
+                    Spacer().frame(height: 220)
+                    
+                }.frame(width: .infinity)
             }
-            
-                        HStack{
-                // H1
-                Text("팀 생성하기")
-                    .font(
-                        Font.custom("Pretendard Variable", size: 20)
-                            .weight(.bold)
-                    )
-                    .kerning(0.02)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color(red: 1, green: 0.44, blue: 0.07)).padding(.trailing, 48).padding(.leading, 26)
-                
-                Image("Arrow").padding(.trailing, 34)
-            }.foregroundColor(.clear)
-                .frame(width: 360, height: 90)
-                .background(.white)
-                .cornerRadius(20)
-                .shadow(color: Color(red: 0.2, green: 0.12, blue: 0.06).opacity(0.1), radius: 10, x: 5, y: 3)
-                .padding(.bottom, 16).padding(.top, 70)
-            
-            HStack(){
-                
-                Text("ETA를 가장 잘 활용하는 방법은 ?")
-                    .font(
-                        Font.custom("Pretendard Variable", size: 20)
-                            .weight(.bold)
-                    )
-                    .kerning(0.02)
-                    .foregroundColor(.black).padding(.leading, 26)
-                Spacer()
-                   
-            }.foregroundColor(.clear)
-                .frame(width: 360, height: 90)
-                .background(.white)
-                .cornerRadius(20)
-                .shadow(color: Color(red: 0.2, green: 0.12, blue: 0.06).opacity(0.1), radius: 10, x: 5, y: 3)
-            Spacer().frame(height: 220)
-            
-        }.frame(width: .infinity)
+        }
     }
-}
 
     struct CollectionViewCell: View {
         var body: some View {
