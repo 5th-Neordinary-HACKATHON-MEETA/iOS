cddf3a32291f4ca052b0b92c50e35ca5183cc2cd
youz2me <kynhun20@gachon.ac.kr>
2023-11-25 14:04:39 +0900

    Build: 프로젝트 초기 세팅

---
+++ ETA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ETA
+//
+//  Created by YOUJIM on 11/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
