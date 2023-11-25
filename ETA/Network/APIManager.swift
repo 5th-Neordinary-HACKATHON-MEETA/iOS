eb599975845c28a975c6a14e54bedd52aea7de4d
youz2me <kynhun20@gachon.ac.kr>
2023-11-26 05:54:41 +0900

    Feat: 로그인 토큰 연결 로직 구현

--- ETA/Network/APIManager.swift
+++ ETA/Network/APIManager.swift
@@ -15,6 +15,10 @@ import Alamofire
 class APIManager: ObservableObject  {
     static let shared = APIManager()
     private var headers: HTTPHeaders?
+    
+    func setHeader(jwt: String) {
+        self.headers = ["Authorization": jwt]
+    }
 }
 
 
@@ -57,10 +61,21 @@ extension APIManager {
         AF
             .request(url, method: .post, parameters: parameter, encoder: .json, headers: self.headers)
             .responseDecodable(of: U.self) { response in
-                //print(response)
                 switch response.result {
                 case .success(let success):
                     completionHandler(success)
+                    
+                    if url == URL(string: "https://eta.jinhy.uk/auth/login") {
+                        if let result = response.response?.allHeaderFields {
+                            if let authorization = result["Authorization"] as? String {
+                                self.setHeader(jwt: authorization)
+                                print("setHeader 성공")
+                                print(authorization)
+                            } else {
+                                print("Authorization 헤더를 찾을 수 없습니다.")
+                            }
+                    }
+                    }
                 case .failure(let error):
                     print(error.localizedDescription)
                 }
