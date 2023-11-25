9a04453179de54ef4247f6f0b2586a5a2e95a86a
youz2me <kynhun20@gachon.ac.kr>
2023-11-25 17:02:16 +0900

    Feat: FontData 추가

---
+++ ETA/Resource/Data/FontData.swift
@@ -0,0 +1,20 @@
+//
+//  FontData.swift
+//  ETA
+//
+//  Created by YOUJIM on 11/25/23.
+//
+
+// MARK: - 피그마에 나와있는 Text styles대로 사용하기 편하게 미리 폰트 선언한 파일
+
+import Foundation
+import UIKit
+
+
+var h1 = UIFont(name: "PretendardVariable-Bold", size: 20)
+var subTitle01 = UIFont(name: "PretendardVariable-Bold", size: 17)
+var subTitle02 = UIFont(name: "PretendardVariable-Bold", size: 14)
+var body01 = UIFont(name: "PretendardVariable-Medium", size: 14)
+var body02 = UIFont(name: "PretendardVariable-Bold", size: 12)
+var body03 = UIFont(name: "PretendardVariable-Medium", size: 12)
+var caption = UIFont(name: "PretendardVariable-Bold", size: 11)
